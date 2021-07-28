a4b5dfc2040ab019df833dbc14b205e6d52d3f7d
Brady Bellini <47389606+bradybellini@users.noreply.github.com>
2021-07-27 21:19:35 -0700

    initial

---
+++ officialmerch/src/routes/index.svelte
@@ -0,0 +1,13 @@
+<script>
+	import supabase from '$lib/db';
+
+	async function getOrgs() {
+		return supabase.from('organization').select(`name`).eq('is_public', true);
+	}
+</script>
+
+{#await getOrgs()}
+	Loading..
+{:then response}
+	<pre>{JSON.stringify(response, null, 2)}</pre>
+{/await}
