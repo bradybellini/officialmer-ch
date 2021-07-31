6998b7f12478862861d94146eed658d4ac94df85
Brady Bellini <47389606+bradybellini@users.noreply.github.com>
2021-07-30 18:07:59 -0700

    Update index.svelte

--- officialmerch/src/routes/index.svelte
+++ officialmerch/src/routes/index.svelte
@@ -1,15 +1,31 @@
 <script>
-
 	import NavBar from '../components/NavBar.svelte';
-	
-	import BaseOrgs from '../components/BaseOrgs.svelte'
+
+	import BaseOrgsCard from '../components/BaseOrgsCard.svelte';
+
 
 	// import { dataset_dev } from 'svelte/internal';
-
-
 </script>
 
-<NavBar></NavBar>
-<BaseOrgs></BaseOrgs>
+
+	<NavBar />
+	<div class="content-containers">
+		<BaseOrgsCard />
+	</div>
 
 
+<style>
+	.content-containers {
+		display: flex;
+		flex-direction: column;
+		/* justify-content: center; */
+		align-items: center;
+		/* padding: 5rem; */
+
+	}
+	
+	/* .main-layout {
+		display: flex;
+		
+	} */
+</style>
